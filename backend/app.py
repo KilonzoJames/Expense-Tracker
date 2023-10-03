5f6aa47e2220e71082917716e7551c50ea09412f
craygenx <marshmellowking99@gmail.com>
2023-10-03 16:19:01 +0300

    created the signup endpoint

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,8 @@ from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_marshmallow import Marshmallow
 from Models.WTFValidationForms.LoginForm import LoginForm
-from werkzeug.security import check_password_hash
+from Models.WTFValidationForms.SignUpForm import SignUpForm
+from werkzeug.security import check_password_hash, generate_password_hash
 from Models.Category import Category
 from Models.Expense import Expense
 from Models.User import User
@@ -36,6 +37,22 @@ def signIn():
         else:
             return jsonify({'error': 'Invalid username or password'}), 401
     return jsonify({'error':'invalid data'}), 400
+
+@app.route('/Signup', methods=['POST'])
+def signUp():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        hashed_password = generate_password_hash(password)
+        newUser = User(username=username, password=hashed_password)
+
+        db.session.add(newUser)
+        db.session.commit()
+
+        return jsonify({'status': 'Registration successful'})
+    return jsonify({'error': form.errors}), 400
 
 
 db.init_app(app)
