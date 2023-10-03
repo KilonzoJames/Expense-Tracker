0233ca6938fa40e0efb305fe5857fe1fa4c8b2e6
KilonzoJames <jay.kilonzo@gmail.com>
2023-10-03 21:04:31 +0300

    Navigation to Homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,6 @@ import Login from './Components/Login';
 import SignUp from './Components/SignUp';
 import Homepage from './Components/Homepage';
 import History from './Components/History';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './Components/Login'
-import SignUp from './Components/SignUp'
 
 function App() {
   return (
--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
 function Login() {
     return (
         <div>
@@ -23,7 +25,8 @@ function Login() {
                                     <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                                 </div>
                                 
-                                <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                                <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"><NavLink to='/homepage'>
+                               Sign in</NavLink></button>
                                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                     Don’t have an account yet? <a href="/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                                 </p>
