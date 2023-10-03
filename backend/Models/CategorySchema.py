c176af1eba170b3909e39150bd752916f00a47b0
craygenx <marshmellowking99@gmail.com>
2023-10-03 14:21:30 +0300

    configured all models mellow schemas

---
+++ backend/Models/CategorySchema.py
@@ -0,0 +1,6 @@
+from ..app import ma
+from Models.Category import Category
+
+class UserSchema(ma.ModelSchema):
+    class Meta:
+        model = Category
--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_cors import CORS
+from flask_marshmallow import Marshmallow
 
 from Models.Config import db
 
 
 app=Flask(__name__)
 CORS(app)
+ma = Marshmallow(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///Tracker.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
