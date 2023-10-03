e39901c16d196fc22f6b6aaf8107f23e6b1ef794
Martin Muriuki <helheim254@gmail.com>
2023-10-03 18:27:28 +0300

    Initial routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login'
+import SignUp from './Components/SignUp'
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+
   )
 }
 
