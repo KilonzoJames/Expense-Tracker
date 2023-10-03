0e0503eb8957c42c2891d121cfb834a817a83d5d
craygenx <marshmellowking99@gmail.com>
2023-10-03 12:15:33 +0300

    added Expense model fields

---
+++ backend/Models/Expense.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime
+from .Config import db
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    descreption = db.Column(db.String(200))
+    amount  = db.Column(db.Float)
+    created_at = db.Column(DateTime, default=datetime.utcnow)
+    updated_at = db.Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    users = db.relationship('UserExpense', backref=db.backref('expenses', cascade='all, delete'))
+
+    def __init__(self, description, amount) -> None:
+        self.descreption = description
+        self.amount = amount
