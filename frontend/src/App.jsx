e3c9a3739b33c4adb66ad341a00ef393220cfee7
Martin Muriuki <helheim254@gmail.com>
2023-10-03 18:09:36 +0300

    Call Login component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import React from 'react'
+import Login from './Components/Login'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Login />
+    </>
   )
 }
 
