e9ad63092e36c1ee953742e32013f80f199b8b56
craygenx <marshmellowking99@gmail.com>
2023-10-03 12:52:47 +0300

    Modified app.py

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,19 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_cors import CORS
+
+from Models.Config import db
+
+
+app=Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///Tracker.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+migrate =Migrate(app, db)
+
+
+db.init_app(app)
+if __name__=='__main__':
+    app.run(port=5555)
