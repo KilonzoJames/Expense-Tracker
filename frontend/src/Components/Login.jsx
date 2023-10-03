52ff711c85d47c74dcaa832d7efc354d51b7a7cf
KilonzoJames <jay.kilonzo@gmail.com>
2023-10-03 19:33:45 +0300

    Frontend

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function SignUp() {
+function Login() {
   return (
     <div>
       
@@ -8,4 +8,4 @@ function SignUp() {
   )
 }
 
-export default SignUp
+export default Login
