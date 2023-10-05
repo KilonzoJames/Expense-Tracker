02259656ca0abe289581d8edfe900eaf109b9b3e
Martin Muriuki <helheim254@gmail.com>
2023-10-05 18:36:45 +0300

    Login and sign up logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,34 @@
-import './App.css'
-import React from 'react'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './Components/Login';
 import SignUp from './Components/SignUp';
 import Homepage from './Components/Homepage';
 import History from './Components/History';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './Components/Login'
-import SignUp from './Components/SignUp'
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
+      {/* <nav>
+        <ul className="flex flex-row">
+          <li>
+            <Link to="/">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+        </ul>
+      </nav> */}
+      <Router>
         <Routes>
-            <Route exact path="/" element={<Login/>} />
-            <Route path='/signup' element={<SignUp/>} />
-            <Route path='/homepage' element={<Homepage/>} />
-            <Route path='/history' element={<History/>} />
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/homepage" element={<Homepage />} />
+          <Route path="/history" element={<History />} />
         </Routes>
-    </BrowserRouter>
-  )
+      </Router>
+    </>
+  );
 }
 
-export default App
-
+export default App;
--- frontend/src/Components/Homepage.jsx
+++ frontend/src/Components/Homepage.jsx
@@ -26,7 +26,7 @@ function Homepage() {
     <>
         <Navbar/>
         <main className='flex items-center justify-center my-12'>
-            <div className='main border-sky-500 cursor-pointer w-96 h-80 shadow-2xl p-6  mx-auto my-12 bg-zinc-100 opacity-0'>
+            <div className='main border-sky-500 cursor-pointer w-96 h-80 shadow-2xl p-6  mx-auto my-12 bg-zinc-100'>
                 <div className='relative left-0 w-full h-1/2 '>
                     <span className='absolute left-0 top-5 text-2xl'>Total Balance</span> 
                     <span className='text-sm absolute right-4 top-2'>KSH</span>
