543af97d2d8992ff86ce19ec9b12a56e6628dbdb
KilonzoJames <jay.kilonzo@gmail.com>
2023-10-09 01:34:26 +0300

    SideNav deleted

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,12 +1,12 @@
 import  { useState, useEffect } from 'react';
-import SideNavbar from './SideNavbar'
 import Navlinks from './Navlinks'
 import { FaSun, FaMoon, FaBars, FaTimes } from 'react-icons/fa';
 import { useTheme } from './ThemeContext';
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
 const { isDarkMode, toggleTheme } = useTheme();
-const [open, setOpen] = useState(true) 
+const [open, setOpen] = useState(false) 
 const modeClass = isDarkMode ? 'dark' : 'light';
 
 useEffect(() => {
@@ -17,13 +17,13 @@ useEffect(() => {
     <div>
         <div className={modeClass}>
             <nav className='navbar fixed top-0 left-0 w-full h-auto px-12 flex justify-between items-center shadow-md md:shadow-lg lg:shadow-xl'>
-                <a href="/history" className='text-4xl font-bold p-4'>Home</a>
-                <Navlinks/>
-                <div className='icons lg:hidden flex items-center gap-10 text-4xl p-4'>
+                <NavLink to="/history" className='text-4xl font-bold p-4'>Home</NavLink>
+                    <Navlinks/>
+                <div className='icons lg:hidden flex items-center gap-10 text-4xl p-4' onClick={() => setOpen(!open)}>
                     {open ? (
-                    <FaBars onClick={() => setOpen(!open)} />
+                    <FaBars />
                     ) : (
-                    <FaTimes onClick={() => setOpen(!open)} />
+                    <FaTimes />
                     )}
                 </div>
                 <div className='text-4xl hover:scale-150 transition-all duration-500' onClick={toggleTheme}>
@@ -31,8 +31,10 @@ useEffect(() => {
                 </div>
             </nav>
         </div>
-        <div className='small lg:hidden' style={{display: open?'none':'block'}}>      
-            <SideNavbar/>
+        <div 
+        className={`lg:hidden bg-slate-400 text-white w-1/2 h-screen lg:h-full gap-24 overflow-y-hidden text-2xl ${open ? 'block' : 'hidden'}`}
+       >      
+            <Navlinks/>
         </div>
     </div>
   )
--- frontend/src/Components/Navlinks.jsx
+++ frontend/src/Components/Navlinks.jsx
@@ -10,28 +10,29 @@ function logoutUser() {
   //     JSON.parse(localStorage.getItem("auth")) || {};
   
   //   return { name, profilePhoto, userID, isAuth };
-  // };
+  // };        // className='lg:hidden'
+
 
 function Navlinks() {
     return (
-    <ul className="hidden lg:flex items-center text-2xl p-4">
-        <li className='text-2xl mx-4 py-4 hover:scale-75 transition-all duration-500'>
+    <div>
+        <div className='text-2xl mx-4 py-4 hover:scale-75 transition-all duration-500'>
             <NavLink to='/history' className='hover:shadow-sm'>
-              Home
-          </NavLink>
-        </li>
-        <li className='text-2xl mx-4 py-4 hover:scale-75 transition-all duration-500'>
+                Home
+            </NavLink>
+        </div>
+        <div className='text-2xl mx-4 py-4 hover:scale-75 transition-all duration-500'>
             <NavLink to='/homepage' className='hover:shadow-sm'>
-              Stats
+                Stats
             </NavLink>
-        </li>
-        <li onClick={logoutUser} className='text-2xl mx-4 py-4 hover:scale-75 transition-all duration-500'>
+        </div>
+        <div onClick={logoutUser} className='text-2xl mx-4 py-4 hover:scale-75 transition-all duration-500'>
             <NavLink to='/' className='hover:shadow-sm'>
-             Log-out
+                Log-out
             </NavLink>
-        </li>
-    </ul>
-      );
-    }
+        </div>
+    </div>
+  );
+}
     
     export default Navlinks;
