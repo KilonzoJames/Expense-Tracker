36283ff152b50129381a8eef1c3f832720d61332
KilonzoJames <jay.kilonzo@gmail.com>
2023-10-08 21:18:11 +0300

    Password maximum length

--- backend/Models/User.py
+++ backend/Models/User.py
@@ -3,7 +3,7 @@ from .Config import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
-    password = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(256), unique=True)
     email = db.Column(db.String(80), unique=True)
 
     expenses = db.relationship('UserExpense', backref=db.backref('users', cascade='all, delete'))
--- backend/Models/WTFValidationForms/SignUpForm.py
+++ backend/Models/WTFValidationForms/SignUpForm.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Length, Email
 
 class SignUpForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=80)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=80)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=256)])
     email = StringField('Email', validators=[DataRequired(), Length(min=4, max=80)])
     
     class Meta:
